ffe9af3dbfe87a02ed4ccdd746aaf45227a33201
Bartosz Szymichowski <bszymi@gmail.com>
2019-05-12 10:48:49 +0100

    update template + get list of steps from map

--- application.py
+++ application.py
@@ -28,9 +28,23 @@ def result():
                                              mode="transit",
                                              departure_time=now)
 
-        print(transit_direction.routes)
+        stepsList = []
 
-        return json.dumps(transit_direction)
+        steps = transit_direction[0]['legs'][0]['steps']
+        for step in steps:
+            distance = step['distance']['value']
+
+            try:
+                travelMode = step['transit_details']['line']['vehicle']['type']
+            except:
+                travelMode = step['travel_mode']
+
+
+            stepsList.append({'distance': distance, 'travelMode': travelMode})
+
+        print(  stepsList)
+
+        return render_template('result.html', start = startLocation, end = endLocation, stepsList = stepsList)
     else :
-        return 'FAIL'
+        return render_template('index.html', message = "Route not found.")
 
