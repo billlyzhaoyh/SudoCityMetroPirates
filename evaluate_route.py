be09d61f9580cd43065eab0fa9fe6ca9f4fdb982
Billy Zhao <billyzhaoyh@gmail.com>
2019-05-12 13:18:20 +0100

    Update

--- evaluate_route.py
+++ evaluate_route.py
@@ -2,28 +2,36 @@
 Evaluate a route based on its carbon footprint.
 """
 from vehicle_footprints import DEFAULT_FOOTPRINT
+from request_mot_data import get_carbon_emission
 
 
 
-def step_cost(step, vehicle_footprints):
+def transit_cost(steplist, vehicle_footprints):
     """
     Return the carbon footprint of a single step of a route in grams.
     """
-    TRAVEL_MODE = 'travel_mode'
+    total_footprint=0
+    carbon_foronetree=60
+    for leg in steplist:
+        leg_type = leg['travelMode']
+        per_km_cost = vehicle_footprints.get(leg_type, DEFAULT_FOOTPRINT)
+        distance = 1e-3 * leg['distance']
+        footprint=distance*per_km_cost
+        total_footprint=total_footprint+footprint
+    days_to_absorb=total_footprint/carbon_foronetree
+    return total_footprint,days_to_absorb
 
-    if step[TRAVEL_MODE] == 'WALKING':
-        vehicle_type = step[TRAVEL_MODE]
-    elif step[TRAVEL_MODE] == 'TRANSIT':
-        vehicle_type = step['transit_details']['line']['vehicle']['type']
-    else:
-        # Handle unknown vehicle types for robustness
-        vehicle_type = 'UNKNOWN'
-
-    per_km_cost = vehicle_footprints.get(vehicle_type, DEFAULT_FOOTPRINT)
-    # Convert meters to kilometers
-    distance = 1e3 * float(step['distance']['value'])
-    # Ignore all other available pieces of data (e.g. duration) for now
-    return per_km_cost * distance
+def drive_cost(distance, licence_plate):
+    """
+    Return the carbon footprint of a single step of a route in grams.
+    """
+    #default api key
+    api='xGJSAEmF0FMRVlFs'
+    per_km_cost=get_carbon_emission(licence_plate, api)
+    total_footprint=per_km_cost*distance*1e-3
+    carbon_foronetree=60
+    days_to_absorb=total_footprint/carbon_foronetree
+    return total_footprint,days_to_absorb
 
 
 def leg_cost_by_steps(leg, vehicle_footprints):
