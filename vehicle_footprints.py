16968fec29bde5073d443539bd68c8776785e9b8
Billy Zhao <billyzhaoyh@gmail.com>
2019-05-12 13:30:06 +0100

    Update

--- vehicle_footprints.py
+++ vehicle_footprints.py
@@ -1,30 +1,31 @@
+
+
 #: Carbon footprint of unknown vehicles in g/km
-DEFAULT_FOOTPRINT = 200.0
+DEFAULT_FOOTPRINT = 68.0
 
 #: CO2 emission of various types of vehicles in g/km.
 #: Expected vehicle types are listed here:
 #: developers.google.com/maps/documentation/directions/intro#VehicleType
 VEHICLE_FOOTPRINTS = {
-    'BUS': 68.0,
-    'RAIL': 14.0,
-    'METRO_RAIL': 43.0,
-    'SUBWAY': 43.0,
-    # TRAM
-    'MONORAIL': 14.0,
-    'HEAVY_RAIL': 14.0,
-    'COMMUTER_TRAIN': 14.0,
-    'HIGH_SPEED_TRAIN': 14.0,
-    'LONG_DISTANCE_TRAIN': 14.0,
-    'BUS': 68.0,
-    'INTERCITY_BUS': 68.0,
-    # TROLLEYBUS
-    # SHARE_TAXI
-    # FERRY
-    # CABLE_CAR
-    # GONDOLA_LIFT
-    # FUNICULAR
-    'OTHER': DEFAULT_FOOTPRINT,
-    # 'walking' is here too for convenience
-    'WALKING': 0.0,
+   'BUS': 68.0,
+   'RAIL': 14.0,
+   'METRO_RAIL': 43.0,
+   'SUBWAY': 43.0,
+   'TRAM': 14.0,
+   'MONORAIL': 14.0,
+   'HEAVY_RAIL': 14.0,
+   'COMMUTER_TRAIN': 14.0,
+   'HIGH_SPEED_TRAIN': 14.0,
+   'LONG_DISTANCE_TRAIN': 14.0,
+   'BUS': 68.0,
+   'INTERCITY_BUS': 68.0,
+   'TROLLEYBUS': 68.0,
+   'SHARE_TAXI': 84.0,
+   'FERRY': 120.0,
+   'CABLE_CAR': 14.0,
+   'GONDOLA_LIFT': 14.0,
+   'FUNICULAR': 14.0,
+   'OTHER': DEFAULT_FOOTPRINT,
+   # 'walking' is here too for convenience
+   'WALKING': 0.0,
 }
-
